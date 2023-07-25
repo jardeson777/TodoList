f8df9e2938e38b0f96c85d50374bd8f65b8dde45
Jardeson <jardeson777@gmail.com>
2023-07-25 14:20:17 -0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>oi</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
